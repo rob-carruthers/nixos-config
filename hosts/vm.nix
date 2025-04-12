130591c2a797e3f0371b0e0b08b331b051de806c
Rob Carruthers <12486527+rob-carruthers@users.noreply.github.com>
2025-04-12 22:31:40 +0100

    efisysmountpoint adjust

--- hosts/vm.nix
+++ hosts/vm.nix
@@ -9,6 +9,7 @@
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.efiSysMountPoint = "/boot/efi";
 
   fileSystems."/" = {
     device = "/dev/disk/by-label/NIXOS_ROOT";
