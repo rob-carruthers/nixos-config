8b68bcda6fe08b58986e371b2ce7056491dcba1b
Rob Carruthers <12486527+rob-carruthers@users.noreply.github.com>
2026-01-11 13:32:13 +0000

    Update helix config (which now includes nixfmt)

--- apps/helix.nix
+++ apps/helix.nix
@@ -4,8 +4,8 @@ let
   helixConfig = pkgs.fetchFromGitHub {
     owner = "rob-carruthers";
     repo = "helix";
-    rev = "c7d5a03";
-    sha256 = "sha256-CDZerHBxzw82k9lyEQrEe9yLKdMygSDuLwrIvGv3A5k=";
+    rev = "321ce71";
+    sha256 = "sha256-+Uf1HDukgMzFv9CvN5DetLLYWUCHKoi+p+oyQl9NzyY=";
   };
 in
 {
