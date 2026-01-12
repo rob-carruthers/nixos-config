d708bd44fe068717e79b966cfb017a9cf1a0ede1
Rob Carruthers <12486527+rob-carruthers@users.noreply.github.com>
2026-01-12 20:42:01 +0000

    We deleted Gentoo, so don't make a boot entry for it ;)

--- hosts/rob-laptop/boot.nix
+++ hosts/rob-laptop/boot.nix
@@ -1,12 +1,5 @@
 {
   boot.loader.limine.extraEntries = ''
-    /Gentoo Linux
-      //6.12.58
-        protocol: linux
-        path: boot():/kernel-6.12.58-gentoo
-        cmdline: root=/dev/mapper/root rootfstype=ext4 rd.luks.name=c5a29596-1e6d-48b7-bfbc-31edef562cec=root acpi_osi=Linux ipv6.disable=1 udev.log_level=3 loglevel=3 systemd.show_status=false rd.udev.log_level=3 pcie_aspm=force module_blacklist=nouveau quiet nowatchdog nmi_watchdog=0 rw module_blacklist=pcspkr acpi.error_level=0x1 acpi.error_log_level=0
-        module_path: boot():/initramfs-6.12.58-gentoo.img
-
     /Arch Linux Install Media
         protocol: linux
         path: boot():/archlinux/vmlinuz-linux
