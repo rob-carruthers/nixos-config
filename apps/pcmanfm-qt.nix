dd8d2e9b9145762b21fa25ea93e262267918dc94
Rob Carruthers <12486527+rob-carruthers@users.noreply.github.com>
2026-01-12 10:42:12 +0000

    Freeze pcmanfm-qt default settings and include in system

--- apps/pcmanfm-qt.nix
+++ apps/pcmanfm-qt.nix
@@ -5,6 +5,8 @@
     lxqt.pcmanfm-qt
   ];
 
+  xdg.configFile."pcmanfm-qt/default/settings.conf".source = ./files/pcmanfm-qt/settings.conf;
+
   home.file."Pictures/background.jpg" = {
     source = ../assets/brighton_pier_sunset-wallpaper-1920x1080.jpg;
   };
