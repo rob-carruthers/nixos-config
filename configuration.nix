08bd3886187a0696642278b531545f6453edde98
Rob Carruthers <12486527+rob-carruthers@users.noreply.github.com>
2026-01-13 12:55:25 +0000

    Remove duplicate openssh directive

--- configuration.nix
+++ configuration.nix
@@ -61,9 +61,6 @@
     wget
   ];
 
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-
   # Copy the NixOS configuration file and link it from the resulting system
   # (/run/current-system/configuration.nix). This is useful in case you
   # accidentally delete configuration.nix.
