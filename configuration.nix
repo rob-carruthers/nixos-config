b2536e73bccde6085ba59d5fe584586d5efa15bb
Rob Carruthers <12486527+rob-carruthers@users.noreply.github.com>
2026-01-14 22:55:38 +0000

    Remove unnecessary comments; redefinitions; imports

--- configuration.nix
+++ configuration.nix
@@ -1,9 +1,4 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
 {
-  config,
   pkgs,
   unstablePkgs,
   ...
@@ -15,19 +10,15 @@
   };
 
   imports = [
-    # Include the results of the hardware scan.
     ./config/boot.nix
     ./config/inputrc.nix
     ./services/avahi.nix
   ];
 
-  # Configure network connections interactively with nmcli or nmtui.
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
   time.timeZone = "Europe/London";
 
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_GB.UTF-8";
 
   programs.dconf.enable = true;
@@ -37,9 +28,6 @@
   users.users.rob = {
     isNormalUser = true;
     extraGroups = [ "wheel" ];
-    #   packages = with pkgs; [
-    #     tree
-    #   ];
   };
 
   programs.bash = {
@@ -47,9 +35,6 @@
     interactiveShellInit = "eval $(starship init bash)";
     loginShellInit = "eval $(starship init bash)";
   };
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
 
   programs.nix-ld.enable = true;
   environment.systemPackages = with pkgs; [
@@ -66,28 +51,6 @@
     wget
   ];
 
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  system.stateVersion = "25.11"; # Did you read the comment?
+  system.stateVersion = "25.11";
 
 }
