f79f5739a5485efb3e2424938dcd32f4b1413e3d
Rob Carruthers <12486527+rob-carruthers@users.noreply.github.com>
2026-01-25 13:41:02 +0000

    Enable PRIME nvidia offload on laptop

--- hosts/rob-laptop/video.nix
+++ hosts/rob-laptop/video.nix
@@ -6,5 +6,13 @@
     nvidiaSettings = true;
     open = true;
     package = config.boot.kernelPackages.nvidiaPackages.production;
+    prime = {
+      offload = {
+        enable = true;
+        enableOffloadCmd = true;
+      };
+      amdgpuBusId = "PCI:35@0:0:0";
+      nvidiaBusId = "PCI:1@0:0:0";
+    };
   };
 }
