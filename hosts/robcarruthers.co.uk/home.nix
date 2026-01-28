87195f727c2aa24f3fbc88083ba27adea46b7480
Rob Carruthers <12486527+rob-carruthers@users.noreply.github.com>
2026-01-28 08:49:43 +0000

    Add font vars to server config (not used, but oh well)

--- hosts/robcarruthers.co.uk/home.nix
+++ hosts/robcarruthers.co.uk/home.nix
@@ -1,9 +1,25 @@
 {
+  pkgs,
+  default-fonts,
+  ...
+}:
+
+let
+  font-sizes = {
+    regular = 8;
+    mono = 9;
+  };
+in
+{
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
   home-manager.users.rob = {
     imports = [
-      ../../users/rob.nix
+      (import ../../users/rob.nix {
+        pkgs = pkgs;
+        default-fonts = default-fonts;
+        font-sizes = font-sizes;
+      })
 
       ../../apps/tmux.nix
     ];
