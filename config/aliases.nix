b43479dd1faf086fa5c1d0d82ecca2054df58681
Rob Carruthers <12486527+rob-carruthers@users.noreply.github.com>
2026-02-02 21:22:56 +0000

    Add nbuild alias

--- config/aliases.nix
+++ config/aliases.nix
@@ -3,5 +3,6 @@
 {
   environment.shellAliases = {
     nswitch = "sudo nixos-rebuild switch --flake .#${config.networking.hostName}";
+    nbuild = "nix build .#nixosConfigurations.${config.networking.hostName}.config.system.build.toplevel";
   };
 }
