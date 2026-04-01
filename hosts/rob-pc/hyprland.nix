c0dae874646fea161a8b11e56f18b292cea12fb4
Rob Carruthers <12486527+rob-carruthers@users.noreply.github.com>
2026-04-01 14:55:15 +0100

    Add hyprland config for rob-pc

--- hosts/rob-pc/hyprland.nix
+++ hosts/rob-pc/hyprland.nix
@@ -1,22 +1,23 @@
 {
   wayland.windowManager.hyprland.settings = {
-    monitor = ",preferred,auto,1";
+    monitor = ",1920x1080@144,auto,1";
     input = {
       kb_layout = "us";
-      touchpad = {
-        natural_scroll = true;
-      };
     };
     exec-once = [
       "ch57x-keyboard-tool upload ~/.config/utility-keys/utility-keys.yaml &"
     ];
+    bind = [
+      # Utility key launchers
+      "$mod CONTROL_L SHIFT, E, exec, ario"
+    ];
     bindel = [
-      ",XF86AudioRaiseVolume, exec, wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"
-      ",XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
-      ",XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
-      ",XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
-      ",XF86MonBrightnessUp, exec, brightnessctl -e4 -n2 set 5%+"
-      ",XF86MonBrightnessDown, exec, brightnessctl -e4 -n2 set 5%-"
+      "$mod CONTROL_L SHIFT, A, exec, mpc prev"
+      "$mod CONTROL_L SHIFT, B, exec, mpc toggle"
+      "$mod CONTROL_L SHIFT, C, exec, mpc next"
+      "$mod CONTROL_L SHIFT, M, exec, pulsemixer --change-volume -2"
+      "$mod CONTROL_L SHIFT, N, exec, pulsemixer --toggle-mute"
+      "$mod CONTROL_L SHIFT, O, exec, pulsemixer --change-volume +2"
     ];
   };
 }
