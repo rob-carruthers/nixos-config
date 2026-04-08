f0505263fae4fbb0a37c06e877d44a61d5320b36
Rob Carruthers <12486527+rob-carruthers@users.noreply.github.com>
2026-04-08 22:13:50 +0100

    Begin a nordic colour scheme for qutebrowser

--- apps/qutebrowser.nix
+++ apps/qutebrowser.nix
@@ -1,5 +1,12 @@
 { default-fonts, font-sizes, ... }:
 
+let
+  colors = {
+    black2 = "#222630";
+    blue2 = "#88C0D0";
+    green_d = "#97B67C";
+  };
+in
 {
   programs.qutebrowser = {
     enable = true;
@@ -10,6 +17,16 @@
       fonts = {
         default_family = default-fonts.mono;
         default_size = toString (font-sizes.mono + 2) + "px";
+      };
+      colors = {
+        tabs = {
+          bar.bg = colors.black2;
+        };
+        statusbar = {
+          normal.bg = colors.black2;
+          normal.fg = colors.green_d;
+          url.fg = colors.blue2;
+        };
       };
     };
   };
