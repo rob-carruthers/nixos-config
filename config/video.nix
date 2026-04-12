bf2007c3da6fdcb6451e7d8aebbba4aeef3587e0
Rob Carruthers <12486527+rob-carruthers@users.noreply.github.com>
2026-04-12 15:22:49 +0100

    Inherit some env vars when launching dwl from greetd

--- config/video.nix
+++ config/video.nix
@@ -1,3 +1,5 @@
+{ pkgs, ... }:
+
 {
   services.xserver.enable = false;
   programs.sway.enable = false;
@@ -6,7 +8,11 @@
   services.greetd.enable = true;
   services.greetd.settings = {
     default_session = {
-      command = "dbus-run-session dwl";
+      command = "dbus-run-session dwl -s ${pkgs.writeShellScript "dwl-session" ''
+        export WAYLAND_DISPLAY
+        systemctl --user import-environment WAYLAND_DISPLAY DISPLAY XDG_CURRENT_DESKTOP
+        systemctl --user start swayidle.service
+      ''}";
       user = "rob";
     };
   };
