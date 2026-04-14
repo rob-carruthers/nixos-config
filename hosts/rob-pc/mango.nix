2c8945fea7ec5c6455f39fa88439f19de91487b5
Rob Carruthers <12486527+rob-carruthers@users.noreply.github.com>
2026-04-14 20:52:49 +0100

    Add some binds and host-specific mango config

--- hosts/rob-pc/mango.nix
+++ hosts/rob-pc/mango.nix
@@ -5,5 +5,16 @@
     borderpx=4
 
     xkb_rules_layout=us
+
+    bind=CTRL+SHIFT+SUPER,a,spawn,mpc prev
+    bind=CTRL+SHIFT+SUPER,b,spawn,mpc toggle
+    bind=CTRL+SHIFT+SUPER,c,spawn,mpc next
+    bind=CTRL+SHIFT+SUPER,e,spawn,ario
+    bind=CTRL+SHIFT+SUPER,m,spawn,pulsemixer --change-volume -2
+    bind=CTRL+SHIFT+SUPER,n,spawn,pulsemixer --toggle-mute
+    bind=CTRL+SHIFT+SUPER,o,spawn,pulsemixer --change-volume +2
+
+    exec-once=ch57x-keyboard-tool upload ~/.config/utility-keys/utility-keys.yaml
+    monitorrule=model:G24-10,refresh:144
   '';
 }
